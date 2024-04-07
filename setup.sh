6f7da042d4e07ba308fc96f6062a0032be78fcc8
Aslan <abulkatim@gmail.com>
2024-04-07 23:08:12 +0500

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,19 +1,14 @@
 #! /bin/bash
 set -xe
 
-#installing stuff
-apt install -y nginx
-apt install -y python3-pip
-apt install -y python3.10-venv
+apt install -y nginx python3-pip python3.10-venv
 /usr/bin/python3 -m venv .venv
 source .venv/bin/activate
 pip3 install -r requirements.txt
 
-#cleanup prev installations
 rm -fv /etc/nginx/sites-available/flask_app
 rm -fv /etc/nginx/sites-enabled/flask_app
 
-#setting stuff up
 cp -r app/html/* /var/www/html/
 cp flask_app /etc/nginx/sites-available/
 ln -s /etc/nginx/sites-available/flask_app /etc/nginx/sites-enabled/
@@ -21,6 +16,11 @@ rm -fv /etc/nginx/sites-enabled/default
 service nginx restart
 
 #launching the broken backend...
-python3 ./app/broken_backend.py &
+cp -f pretty_backend.service /etc/systemd/system/pretty_backend.service
 
-#correct backend file is interview_backend.py
+cp -f interview-backend.service /etc/systemd/system/interview-backend.service
+
+chmod 644 /etc/systemd/system/interview-backend.service
+chmod 644 /etc/systemd/system/pretty_backend.service 
+
+systemctl start pretty_backend.service
