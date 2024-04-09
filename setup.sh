be783cba3bf134cc0585e10010edc6b0dce18534
Aslan <abulkatim@gmail.com>
2024-04-09 12:51:56 +0500

    Update as discussed

--- setup.sh
+++ setup.sh
@@ -1,32 +1,30 @@
 #! /bin/bash
 set -xe
 
-systemctl stop interview-backend.service
-systemctl stop pretty_backend.service
-
+# Setup
 apt install -y nginx python3-pip python3.10-venv
 /usr/bin/python3 -m venv .venv
 source .venv/bin/activate
 pip3 install -r requirements.txt
 
+
+# Rollback to the beginning
+systemctl stop interview-backend.service
 rm -fv /etc/nginx/sites-available/flask_app
 rm -fv /etc/nginx/sites-enabled/flask_app
-
 cp -r app/html/* /var/www/html/
 cp flask_app /etc/nginx/sites-available/
 ln -s /etc/nginx/sites-available/flask_app /etc/nginx/sites-enabled/
 rm -fv /etc/nginx/sites-enabled/default
-service nginx restart
-
-cp -f interview-backend.service /etc/systemd/system/interview-backend.service
-
-chmod 644 /etc/systemd/system/interview-backend.service
-
 rm -rf /home/hero/*
 cp strange_box.tar.gz /home/hero/strange_box.tar.gz
+cp -f interview-backend.service /etc/systemd/system/interview-backend.service
+chmod 644 /etc/systemd/system/interview-backend.service
 
-systemctl daemon-reload
-
+# Start of quest
 bash ./broken_backend.sh
-systemctl stop interview-backend.service
+systemctl daemon-reload
+systemctl start interview-backend.service
+service nginx restart
+
 
